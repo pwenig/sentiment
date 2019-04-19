90ef0e7724002913cea4cf2305d332c151a689f7
Paul Wenig <pwenig@gmail.com>
2019-04-19 11:19:49 -0600

    Enable serach term

--- lib/sentiment_parser.rb
+++ lib/sentiment_parser.rb
@@ -6,21 +6,23 @@ require 'pry'
 class SentimentParser
   #
   def initialize
+    puts "Enter your search word"
+    search_term = gets.chomp
+
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
       config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
       config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
       config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
     end
-    search
+    search(search_term)
   end
 
-  def search
-    @client.search("to:justinbieber marry me", result_type: "recent").take(3).each do |tweet|
+  def search(search_term)
+    @client.search(search_term).take(10).each do |tweet|
       puts tweet.text
     end
   end
-
 
 end
 
