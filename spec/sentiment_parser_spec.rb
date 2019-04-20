94963eeada850bd9db4089ed3ca9028fd38bc98e
Paul Wenig <pwenig@gmail.com>
2019-04-20 12:29:31 -0600

    Add specs

--- spec/sentiment_parser_spec.rb
+++ spec/sentiment_parser_spec.rb
@@ -3,6 +3,17 @@ require 'sentiment_parser'
 
 describe SentimentParser do
 
+  before do
+    @results = File.read('spec/fixtures/twitter.txt')
+    @analysis = {positive: 183, negative: 266}
+  end
 
+  it 'returns results' do
+    expect(@results.length).to_not eq(0)
+  end
 
+  it 'analyzes' do
+    analyze = SentimentParser.analyze(@results)
+    expect(analyze).to eq @analysis
+  end
 end
