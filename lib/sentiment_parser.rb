60166aa553e1a44d724c39ee5b5f5dadbcf11d57
Paul Wenig <pwenig@gmail.com>
2019-04-20 12:31:21 -0600

    Edit file

--- lib/sentiment_parser.rb
+++ lib/sentiment_parser.rb
@@ -1,33 +1,5 @@
-require 'dotenv'
-Dotenv.load("./.env")
-require 'twitter'
 require 'pry'
 
 class SentimentParser
 
-  SEARCH_TERM = ''
-
-  def initialize
-    twitter_connect
-    search
-  end
-
-  def twitter_connect
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
-      config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
-    end
-  end
-
-  def search
-    tweets = []
-    @client.search(SEARCH_TERM).take(500).each do |tweet|
-      tweets << tweet.text
-      puts tweet.text
-    end
-  end
 end
-
-SentimentParser.new
