5a0a4ac94759fdf89457171634a3d029e40a2c7f
Paul Wenig <pwenig@gmail.com>
2019-04-23 08:17:52 -0600

    Add extra credit

--- spec/sentiment_parser_spec.rb
+++ spec/sentiment_parser_spec.rb
@@ -12,8 +12,25 @@ describe SentimentParser do
     expect(@results.length).to_not eq(0)
   end
 
-  it 'analyzes' do
+  xit 'returns positive words' do
+    results = SentimentParser.positive_words
+    expect(results.length).to_not eql 0
+  end
+
+  xit 'returns negative words' do
+    results = SentimentParser.negative_words
+    expect(results.length).to_not eql 0
+  end
+
+  xit 'analyzes' do
     analyze = SentimentParser.analyze(@results)
     expect(analyze).to eq @analysis
   end
+
+#   Extra credit
+#   Write a test that calls a method "top_words" that returns
+#   a hash that has an array of hashes with the top three positive and negative words
+#   and counts
+#   top_words = { positive: [ {word1: 10}, {word2: 7}, {word3: 5} ], negative: [ {word1: 7},
+#       {word2: 5}, {word3: 2} ] }
 end
